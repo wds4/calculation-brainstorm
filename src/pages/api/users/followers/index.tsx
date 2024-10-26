f6f526987c7c59283d1e3d5a8a9a84c74c4251e2
David Strayhorn <david.strayhorn4@gmail.com>
2024-10-26 18:15:22 -0400

    update followers endpoint

--- src/pages/api/users/followers/index.tsx
+++ src/pages/api/users/followers/index.tsx
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from "@vercel/postgres";
 import { verifyPubkeyValidity } from '@/helpers/nip19';
-import { RatingsCV0o, RatingsWithMetaDataCV0o } from '@/types';
+// import { RatingsCV0o, RatingsWithMetaDataCV0o } from '@/types';
 import { arrayToObject } from '@/helpers';
 
 /*
@@ -11,6 +11,7 @@ http://localhost:3000/api/users/followers?pubkey=e5272de914bd301755c439b88e6959a
 https://calculation-brainstorm.vercel.app/api/users/followers?pubkey=e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f
 */
 
+/*
 const fetchFollowers = (oRatingsWithMetaData:RatingsWithMetaDataCV0o, pubkey1:string, oPubkeyLookupByUserId:{[key:string]: {[key: string]: string}}, oUserIdLookupByPubkey:{[key:string]: {[key:string]:number}}) => {
   const userId1 = oUserIdLookupByPubkey[pubkey1].id
   console.log(`typeof userId1: ${typeof userId1}`)
@@ -43,6 +44,7 @@ const fetchFollowers = (oRatingsWithMetaData:RatingsWithMetaDataCV0o, pubkey1:st
   }
   return { aFollowers, aMuters }
 }
+*/
 
 type ResponseData = {
   success: boolean,
@@ -55,9 +57,6 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   const searchParams = req.query
-  if (searchParams.npub) {
-    // TODO: support npub
-  }
   if (!searchParams.pubkey) {
     const response:ResponseData = {
       success: false,
@@ -65,15 +64,53 @@ export default async function handler(
     }
     res.status(200).json(response)
   }
+  // TODO: need 2 params, one for the customer and one for the target
+  // need customer pubkey to fetch from ratingsTables 
+  // alternatively: create followers in a customer-agnostic way <== this 
   if (searchParams.pubkey) {
     const pubkey1 = searchParams.pubkey
     if ((typeof pubkey1 == 'string') && (verifyPubkeyValidity(pubkey1)) ) {
       const client = await db.connect();
       try {
-        const result_exists = await client.sql`SELECT EXISTS(SELECT 1 FROM users WHERE pubkey=${pubkey1}) AS "exists"`
-        console.log(result_exists)
-        if (result_exists.rows[0].exists == true) {
+        const result_pubkey1 = await client.sql`SELECT id, pubkey FROM users WHERE pubkey=${pubkey1}`
+        if (result_pubkey1.rowCount) {
+          const id1 = result_pubkey1.rows[0].id
           // calculated followers and muters for this profile
+          const result_allUsers = await client.sql`SELECT id, pubkey, whenlastcreatedobserverobject, observerobject FROM users`;
+          const oPubkeyLookupByUserId = arrayToObject(result_allUsers.rows, 'id')
+          const aFollowers = []
+          const aMuters = []
+          for (let u=0; u < result_allUsers.rows.length; u++) {
+            const whenLastCreatedObserverObject = result_allUsers.rows[u].whenlastcreatedobserverobject
+            if (whenLastCreatedObserverObject) {
+              const oObserverObject = result_allUsers.rows[u].observerobject              
+              const id_rater = result_allUsers.rows[u].id
+              const pk_rater = oPubkeyLookupByUserId[id_rater].pubkey
+              if (oObserverObject[id_rater].hasOwnProperty(id1)) {
+                const placeholder = oObserverObject[id_rater][id1]
+                if (placeholder == 'f') {
+                  aFollowers.push(pk_rater)
+                }
+                if (placeholder == 'm') {
+                  aMuters.push(pk_rater)
+                }
+              }
+            }
+          }
+          const response:ResponseData = {
+            success: true,
+            message: `grapevine calculation engine users/followers api for pubkey ${pubkey1}:`,
+            data: {
+              numUsers: result_allUsers.rows.length,
+              numFollowers: aFollowers.length,
+              numMuters: aMuters.length,
+              aFollowers,
+              aMuters
+            }
+          }
+          res.status(200).json(response)
+
+          /*
           const resultUsers = await client.sql`SELECT id, pubkey FROM users`;
           const oPubkeyLookupByUserId = arrayToObject(resultUsers.rows, 'id')
           const oUserIdLookupByPubkey = arrayToObject(resultUsers.rows, 'pubkey')
@@ -94,11 +131,13 @@ export default async function handler(
             }
             res.status(200).json(response)
           }
+
           const response:ResponseData = {
             success: true,
             message: `grapevine calculation engine users/followers api: pubkey ${pubkey1} exists in the calculation engine users database`,
           }
           res.status(200).json(response)
+          */
         } else {
           const response:ResponseData = {
             success: false,
