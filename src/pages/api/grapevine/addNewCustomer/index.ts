a0d2b26ae1a9eff3aab41e3c6602def551b009cd
David Strayhorn <david.strayhorn4@gmail.com>
2024-10-26 22:05:30 -0400

    updated followers endpoint; set signup to trigger listener

--- src/pages/api/grapevine/addNewCustomer/index.ts
+++ src/pages/api/grapevine/addNewCustomer/index.ts
@@ -71,6 +71,13 @@ export default async function handler(
           await client.sql`INSERT INTO dosSummaries (pubkey, customerid, userid) VALUES (${pubkey1}, ${customerID}, ${userID}) ON CONFLICT DO NOTHING;`
           await client.sql`INSERT INTO ratingsTables (pubkey, customerid) VALUES (${pubkey1}, ${customerID}) ON CONFLICT DO NOTHING;`
           await client.sql`INSERT INTO scorecardsTables (pubkey, customerid) VALUES (${pubkey1}, ${customerID}) ON CONFLICT DO NOTHING;`
+
+          const url = `https://interpretation-brainstorm.vercel.app/api/nostr/listeners/singleUser?pubkey=${pubkey1}`
+          const triggerNextEndpoint = (url:string) => {
+            fetch(url)
+          }
+          triggerNextEndpoint(url)
+
           const response:ResponseData = {
             success: true,
             message: `pubkey ${pubkey1} inserted successfully into the customer, user, dosSummaries, and ratingsTables tables`,
--- src/pages/api/users/followers/index.tsx
+++ src/pages/api/users/followers/index.tsx
@@ -9,41 +9,7 @@ to access:
 pubkey: e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f
 http://localhost:3000/api/users/followers?pubkey=e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f
 https://calculation-brainstorm.vercel.app/api/users/followers?pubkey=e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f
-*/
 
-/*
-const fetchFollowers = (oRatingsWithMetaData:RatingsWithMetaDataCV0o, pubkey1:string, oPubkeyLookupByUserId:{[key:string]: {[key: string]: string}}, oUserIdLookupByPubkey:{[key:string]: {[key:string]:number}}) => {
-  const userId1 = oUserIdLookupByPubkey[pubkey1].id
-  console.log(`typeof userId1: ${typeof userId1}`)
-  const oRatingsIn:RatingsCV0o = oRatingsWithMetaData.data
-  const aFollowers:string[] = []
-  const aMuters:string[] = []
-  const ratingsMetaData = oRatingsWithMetaData.metaData
-  if (ratingsMetaData?.compactFormat) {
-    const context = 'notSpam'
-    const aRaters = Object.keys(oRatingsIn[context])
-    for (let r=0; r < aRaters.length; r++) {
-      const rater:string = aRaters[r]
-      const aRatees = Object.keys(oRatingsIn[context][rater])
-      for (let z=0; z < Math.min(aRatees.length, 1000); z++) {
-        const ratee:string = aRatees[z]
-        // if ((r < 5) && (z < 5)) { console.log(`typeof ratee: ${typeof ratee}`) }
-        if (Number(ratee) == userId1) {
-          // console.log(`FOUND A RATER!!!: ${rater}`)
-          const pk_rater = oPubkeyLookupByUserId[rater].pubkey
-          const placeholder:string = oRatingsIn[context][rater][ratee]
-          if (placeholder == 'f') {
-            aFollowers.push(pk_rater)
-          }
-          if (placeholder == 'm') {
-            aMuters.push(pk_rater)
-          }
-        }
-      }
-    }
-  }
-  return { aFollowers, aMuters }
-}
 */
 
 type ResponseData = {
